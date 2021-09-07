f92a3dc4c6b254f8309256489a990248ee2f673b
seonja <seonja.kim0@gmail.com>
2021-09-07 09:38:25 +0900

    lower the level of abstraction and remove all the components except SquareButton

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,59 +1,25 @@
 import React, { useState } from 'react'
-import Container from '../components/Container'
-import RoundButton from '../components/RoundButton'
-import InputWindow from '../components/InputWindow'
 import SpinboxCard from './midAbstraction/SpinboxCard'
+import SquareButton from '../components/SquareButton'
 
 const App = () => {
-  const [newSpinbox, setNewSpinbox] = useState([[1, 2, 3]])
-  const [input, setInput] = useState([1, 2, 3])
+  const [newSpinbox, setNewSpinbox] = useState([1])
 
-  /** check the order of spinbox and make new spinbox */
-  const checkInputAddSpinbox = () => {
-    if (input && input.some((el) => el < '1' || el > '3')) {
-      alert('please put number from 1 to 3')
-    } else {
-      setNewSpinbox([...newSpinbox, input])
-    }
-  }
-  /** when there is no input, set initial value as [1, 2, 3] */
-  const getUserInput = (userInput) => {
-    if (!userInput) {
-      setInput([1, 2, 3])
-    } else {
-      setInput(userInput.split(''))
-    }
-  }
-
-  const createSpinboxes = newSpinbox.map((el, idx) => (
-    <SpinboxCard key={idx} order={el} />
-  ))
-  const addSpinboxButtonName = (
-    <>
-      <div>Add</div>
-      <div>Spinbox</div>
-    </>
-  )
-  const bottomAreaItems = (
-    <>
-      <InputWindow
-        placeholder="rearrange spinbox ex) 213"
-        size="small"
-        onChange={getUserInput}
-      />
-      <RoundButton
-        onClick={checkInputAddSpinbox}
-        buttonName={addSpinboxButtonName}
-        emphasis="low"
-      />
-    </>
-  )
+  const createSpinboxes = newSpinbox.map((el, idx) => <SpinboxCard key={idx} />)
 
   return (
-    <Container
-      upperArea={createSpinboxes}
-      bottomArea={bottomAreaItems}
-    ></Container>
+    <div className="container-background">
+      <div className="spinbox-container">
+        <div className="spinbox-window">{createSpinboxes}</div>
+        <div className="add-spinbox-area">
+          <SquareButton
+            onClick={() => setNewSpinbox([...newSpinbox, 1])}
+            buttonName="more spinbox"
+            emphasis="low"
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 export default App
--- src/pages/midAbstraction/SpinboxCard.js
+++ src/pages/midAbstraction/SpinboxCard.js
@@ -1,15 +1,12 @@
 import React, { useReducer } from 'react'
-import InputWindow from '../../components/InputWindow'
 import SquareButton from '../../components/SquareButton'
-import useLongPress from '../../library/useLongPress'
 import useLongPressCompare from '../../library/useLongPressNoUseEffect'
 
 /**
  * each spinbox should work separately
  * so that this component contains its own functions
  */
-const SpinboxCard = ({ order }) => {
-  /** could define it outside of this scope for reusability */
+const SpinboxCard = () => {
   const reducer = (number, action) => {
     switch (action.type) {
       case 'INCREMENT':
@@ -32,39 +29,27 @@ const SpinboxCard = ({ order }) => {
   const longPressIncreaseCompare = useLongPressCompare(increase, 1000)
   const longPressDecreaseCompare = useLongPressCompare(decrease, 1000)
 
-  // leave this for the test purpose
-  const longPressIncrease = useLongPress(increase, 1000)
-  const longPressDecrease = useLongPress(decrease, 1000)
-
-  const spinboxList = {
-    1: <InputWindow key="1" number={number} onChange={dispatchNumber} />,
-    2: (
+  return (
+    <div className="card-container">
+      <input
+        value={number}
+        onChange={dispatchNumber}
+        className="input-window"
+      />
       <SquareButton
-        key="2"
         emphasis="high"
         onClick={increase}
         useLongPress={longPressIncreaseCompare}
-        buttonName={<ArrowIcon up="up" />}
+        buttonName="+"
       />
-    ),
-    3: (
       <SquareButton
-        key="3"
         emphasis="high"
         onClick={decrease}
         useLongPress={longPressDecreaseCompare}
-        buttonName={<ArrowIcon />}
+        buttonName="-"
       />
-    ),
-  }
-  const spinboxOrder = order.map((el) => spinboxList[el])
-
-  return <div className="card-container">{spinboxOrder}</div>
-}
-
-// the arrow icon in the square button
-const ArrowIcon = ({ up }) => {
-  return <div className={`arrow-i ${up}`}></div>
+    </div>
+  )
 }
 
 export default SpinboxCard
